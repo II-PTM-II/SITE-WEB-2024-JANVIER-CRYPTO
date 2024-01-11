595ca24bf317eb489a5db66183e1497210848905
Karuran12 <156071584+Karuran12@users.noreply.github.com>
2024-01-11 23:08:51 +0100

    Add files via upload

--- REAL WEB 2/Site/style/wj.js
+++ REAL WEB 2/Site/style/wj.js
@@ -14,4 +14,40 @@ videos.forEach(video => {
     observer.observe(video);
 });
 
+const ctx2 = document.getElementById('doughnut').getContext('2d');
+const doughnut = new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+        labels: ['public sale', 'exchange and liquidity', 'marketing', 'community rewards', 'charity', 'development', 'team'],
+        datasets: [{
+            label: '# of Votes',
+            data: [45, 10, 15, 5, 10, 10, 5],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(212, 189, 222, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1,
 
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
